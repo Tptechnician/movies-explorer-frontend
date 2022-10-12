73ecc0fe2860b2ae92209082e888dc78cc1c3df0
Pavel Demin <deminps.2008@gmail.com>
2022-10-12 22:42:46 +0500

    HTML-markup: Component/about-project

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -2,7 +2,37 @@ import React from 'react';
 import './AboutProject.css';
 
 function AboutProject() {
-  return <div>hello world</div>;
+  return (
+    <section id='about-project' className='about-project'>
+      <h2 className='about-project__title'>О проекте</h2>
+      <ul className='about-project__info'>
+        <li className='about-project__info__list'>
+          <h3 className='about-project__info__list__title'>Дипломный проект включал 5 этапов</h3>
+          <p className='about-project__info__list__subtitle'>
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
+            доработки.
+          </p>
+        </li>
+        <li className='about-project__info__list'>
+          <h3 className='about-project__info__list__title'>На выполнение диплома ушло 5 недель</h3>
+          <p className='about-project__info__list__subtitle'>
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
+            успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <div className='about-project__time-bar'>
+        <div className='about-project__time-bar__first-column'>
+          <p className='about-project__time-bar__first-column__time'>1 неделя</p>
+          <p className='about-project__time-bar__first-column__subtitle'>Back-end</p>
+        </div>
+        <div className='about-project__time-bar__second-column'>
+          <p className='about-project__time-bar__second-column__time'>4 недели</p>
+          <p className='about-project__time-bar__second-column__subtitle'>Front-end</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutProject;
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Promo.css';
 import promoLogo from '../../images/promo-landing-logo.svg';
 
 function Promo() {
   return (
-    <div className="promo">
-      <div className="promo__coteiner-info">
-        <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
-        <p className="promo__subtitle">
+    <section className='promo'>
+      <div className='promo__coteiner-info'>
+        <h1 className='promo__title'>Учебный проект студента факультета Веб&#8209;разработки.</h1>
+        <p className='promo__subtitle'>
           Листайте ниже, чтобы узнать больше про этот проект и его создателя.
         </p>
-        <Link className="promo__link" href="#aboutProject">
+        <a className='promo__link' href='#about-project'>
           Узнать больше
-        </Link>
+        </a>
       </div>
-      <img className="promo__logo" src={promoLogo} alt="Logo" />
-    </div>
+      <img className='promo__logo' src={promoLogo} alt='Logo' />
+    </section>
   );
 }
 
