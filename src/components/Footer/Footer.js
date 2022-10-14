538f5b23f990cb84cee9751f47a61f41b7ad37db
Pavel Demin <deminps.2008@gmail.com>
2022-10-14 18:27:52 +0500

    HTML-markup: Component/Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,36 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <div className='footer'>hello world</div>;
+  return (
+    <footer className='footer'>
+      <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__conteiner'>
+        <p className='footer__copyright'>&copy; 2022</p>
+        <ul className='footer__links'>
+          <li className='footer__link-item'>
+            <a
+              className='footer__link'
+              href='https://praktikum.yandex.ru/'
+              target={'_blank'}
+              rel={'noreferrer'}
+            >
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li className='footer__link-item'>
+            <a
+              className='footer__link'
+              href='https://github.com/Tptechnician'
+              target={'_blank'}
+              rel={'noreferrer'}
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
